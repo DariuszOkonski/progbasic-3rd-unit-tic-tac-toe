404017d2f74379bc7eec80cc98ef851c687731b7
Dariusz Okonski <darek200180@gmail.com>
2020-12-09 23:52:10 +0000

    built basics of different players options

--- part_three.py
+++ part_three.py
@@ -16,19 +16,15 @@ def print_result(winner):
         print("O has won!")
     else:
         print("It's a tie!")
+    
 
+def display_result(player, board):
+    clear_console()
+    print_result(player)
+    print_board(board)
+    return True
 
-def tictactoe_game(mode='HUMAN-HUMAN'):
-    # board = init_board()
-
-    # # use get_move(), mark(), has_won(), is_full(), and print_board() to create game logic
-    # print_board(board)
-    # row, col = get_move(board, 1)
-    # mark(board, 1, row, col)
-
-    # winner = '0'
-    # print_result(winner)
-
+def mode_human_human(mode):
     board = init_board()
     isPlayerChanged = True
     end_game = False
@@ -46,21 +42,23 @@ def tictactoe_game(mode='HUMAN-HUMAN'):
         mark(board, player, row, col)
 
         if(has_won(board, player)):
-            clear_console()
-            print_result(player)
-            print_board(board)
-            end_game = True
+            end_game = display_result(player, board)
         elif is_full(board):
-            clear_console()
-            print_result('0')
-            print_board(board)
-            end_game = True
-
+            player = '0'
+            end_game = display_result(player, board)
         isPlayerChanged = not isPlayerChanged
+    
+    return end_game
 
-    print("=== END GAME")
 
-        # break
+def tictactoe_game(mode='HUMAN-HUMAN'):
+    options = {'human-human': 'HUMAN-HUMAN'}
+
+
+    if mode == options['human-human']:
+        mode_human_human(mode)
+    else:
+        print("other options")
 
 
 
