96725765fa64fda20c9986b9d6aea0b6aac3ce7b
Dariusz Okonski <darek200180@gmail.com>
2020-12-09 14:18:22 +0000

    finish function get_move with all requirements

--- part_one.py
+++ part_one.py
@@ -1,4 +1,4 @@
-def get_proper_coordinates(row_dictionary, column_dictionary):
+def get_proper_coordinates(row_dictionary, column_dictionary, board):
     isCoordinatesCorrect = False
     while not isCoordinatesCorrect:
         coordinates = input("Please enter coordinates: ").upper()
@@ -18,7 +18,19 @@ def get_proper_coordinates(row_dictionary, column_dictionary):
         else:
             print("Some coordinates are wrong")
 
+        if isCoordinatesCorrect:
+            input_row = row_dictionary[input_row]
+            input_col = column_dictionary[input_col]
+            isCoordinatesCorrect = check_if_position_in_board_is_empty(input_row, input_col, board)
+
     return (input_row, input_col)
+
+def check_if_position_in_board_is_empty(row, col, board):
+    if(board[row][col] == '.'):
+        return True
+    else:
+        print("Current position is occupied")
+        return False
 
 # ==========================================================================
 def init_board():
@@ -35,56 +47,17 @@ def init_board():
 
 def get_move(board, player):
     """Returns the coordinates of a valid move for player on board."""
-    row_dictionary = { 'A': 0, 'B': 1, 'C': 2}
+    row_dictionary = {'A': 0, 'B': 1, 'C': 2}
     column_dictionary = {'1': 0, '2': 1, '3': 2}
 
-    row, col = 0, 0
-    print(board)
-
-    # isCoordinatesCorrect = False
-    # while not isCoordinatesCorrect:
-    #     coordinates = input("Please enter coordinates: ").upper()
-    #
-    #     if(len(coordinates) != 2):
-    #         print("Something is wrong, Please repeat your coordinates")
-    #         continue
-    #
-    #     input_row = coordinates[0:1]
-    #     input_col = coordinates[1]
-    #
-    #     is_input_row_in_dictionary = input_row in row_dictionary
-    #     is_input_col_in_dictionary = input_col in column_dictionary
-    #
-    #     if is_input_row_in_dictionary and is_input_col_in_dictionary:
-    #         print("coordinates are ok")
-    #         isCoordinatesCorrect = True
-    #     else:
-    #         print("Some coordinates are wrong")
-
-
-    row_position, col_position = get_proper_coordinates(row_dictionary, column_dictionary)
-    print(row_position, col_position)
-
-
-    # if coordinates are out of board, keep asking
-    # if coordinates are already taken, keep asking
-    # if coordinates doesn't look like coordinates, keep asking
-
-
-
-
-    # player specifies coordinates i.e: A2, C1
-    # return tuple of tow int (row, col) - coordinates starts from 0
-    # coordinates has to indicate a valid empty position in board
-
-
-
+    row, col = get_proper_coordinates(row_dictionary, column_dictionary, board)
 
     return row, col
 
 def mark(board, player, row, col):
     """Marks the element at row & col on the board for player."""
     pass
+
 
 #
 board = init_board()
@@ -94,4 +67,4 @@ board[1][1] = 'x'
 board[2][0] = "o"
 
 row, col = get_move(board, 'player1')
-# print(row, col)
+print(row, col)
